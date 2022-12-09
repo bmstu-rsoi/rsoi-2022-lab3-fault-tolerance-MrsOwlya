a232d99a00396dc1b1777c2bf24a05cf5b66d5bc
MrsOwlya <olja-holmes@mail.ru>
2022-12-09 06:29:42 +0300

    try to find bug ports 2

--- scripts/wait-script.sh
+++ scripts/wait-script.sh
@@ -3,16 +3,16 @@
 IFS="," read -ra PORTS <<<"$WAIT_PORTS"
 path=$(dirname "$0")
 
-curl -o /dev/null -s -w "http://localhost:8080/manage/health: %{http_code}\n" http://localhost:8080/manage/health
-curl -o /dev/null -s -w "http://localhost:8070/manage/health: %{http_code}\n" http://localhost:8070/manage/health
-curl -o /dev/null -s -w "http://localhost:8060/manage/health: %{http_code}\n" http://localhost:8060/manage/health
-curl -o /dev/null -s -w "http://localhost:8050/manage/health: %{http_code}\n" http://localhost:8050/manage/health
-
 PIDs=()
 for port in "${PORTS[@]}"; do
   "$path"/wait-for.sh -t 120 "http://localhost:$port/manage/health" -- echo "Host localhost:$port is active" &
   PIDs+=($!)
 done
+
+curl -o /dev/null -s -w "http://localhost:8080/manage/health: %{http_code}\n" http://localhost:8080/manage/health
+curl -o /dev/null -s -w "http://localhost:8070/manage/health: %{http_code}\n" http://localhost:8070/manage/health
+curl -o /dev/null -s -w "http://localhost:8060/manage/health: %{http_code}\n" http://localhost:8060/manage/health
+curl -o /dev/null -s -w "http://localhost:8050/manage/health: %{http_code}\n" http://localhost:8050/manage/health
 
 for pid in "${PIDs[@]}"; do
   if ! wait "${pid}"; then
