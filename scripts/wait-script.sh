db510f5af965706e0e85326da47bde105dcd9b69
MrsOwlya <olja-holmes@mail.ru>
2022-12-09 06:23:22 +0300

    try to find bug ports

--- scripts/wait-script.sh
+++ scripts/wait-script.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
-echo "AAAAAAAAAAAAAAAAAAAAAAAAAAA"
+
 IFS="," read -ra PORTS <<<"$WAIT_PORTS"
 path=$(dirname "$0")
+
+curl -o /dev/null -s -w "http://localhost:8080/manage/health: %{http_code}\n" http://localhost:8080/manage/health
+curl -o /dev/null -s -w "http://localhost:8070/manage/health: %{http_code}\n" http://localhost:8070/manage/health
+curl -o /dev/null -s -w "http://localhost:8060/manage/health: %{http_code}\n" http://localhost:8060/manage/health
+curl -o /dev/null -s -w "http://localhost:8050/manage/health: %{http_code}\n" http://localhost:8050/manage/health
 
 PIDs=()
 for port in "${PORTS[@]}"; do
