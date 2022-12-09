e71fd5e79f3aa9e53281e9f7ed46f22e3f38a0a0
MrsOwlya <olja-holmes@mail.ru>
2022-12-09 04:54:45 +0300

    manage health

--- FlightService/FlightService/urls.py
+++ FlightService/FlightService/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('flight.urls')),
+    path('manage/health', include('flight.url_manage'))
 ]
--- TicketService/TicketService/urls.py
+++ TicketService/TicketService/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('ticket.urls')),
+    path('manage/health', include('ticket.url_manage'))
 ]
